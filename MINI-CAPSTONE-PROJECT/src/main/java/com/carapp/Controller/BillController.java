d03720d27135a9b4de3e1f2af14b606f78bc9221
supriya <saisupriyadevireddy046@gmail.com>
2022-11-12 10:27:24 +0530

    Second Commit

---
+++ MINI-CAPSTONE-PROJECT/src/main/java/com/carapp/Controller/BillController.java
@@ -0,0 +1,42 @@
+package com.carapp.Controller;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/bill")
+public class BillController {
+
+	@Autowired
+	private ICarService service;
+
+	public void setService(ICarService service) {
+		this.service = service;
+	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "/generate", method = RequestMethod.GET)
+	public   ResponseEntity generateBill(@RequestParam int checkInId) throws ParseException {
+		int result = service.generateBill(checkInId);
+		ResponseEntity resp = new ResponseEntity(200, result, "");
+		return resp;
+	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "/pay/{checkInId}", method = RequestMethod.GET)
+	public   ResponseEntity payBill(@PathVariable int checkInId) {
+		String result = service.updatePayBill(checkInId);
+		ResponseEntity resp = new ResponseEntity(200, result, "");
+		return resp;
+	}
+	
+}
+
---
+++ MINI-CAPSTONE-PROJECT/src/main/java/com/carapp/Controller/ICarService.java
@@ -0,0 +1,17 @@
+package com.carapp.Controller;
+
+import org.springframework.util.MultiValueMap;
+
+public class ICarService {
+
+	public int generateBill(int checkInId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public MultiValueMap updatePayBill(int checkInId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
